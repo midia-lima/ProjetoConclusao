1eb10eb9d930f7a443d5e24ac68a3ed8878cb54d
midia-lima <midialp2@gmail.com>
2020-06-23 21:22:10 -0300

    projeto de Conclusão

---
+++ js/main.js
@@ -0,0 +1,15 @@
+function ativaScrollSuave(selector) {
+
+    $(selector).click(function(event){
+        event.preventDefault();
+        var target = $(this).attr('href');
+
+        $('html, body').animate({
+            scrollTop: $(target).offset().top
+        }, 1000)
+    });
+}
+
+ativaScrollSuave('a[href*=sobre]')
+ativaScrollSuave('a[href*=sobre-palestrantes]')
+ativaScrollSuave('a[href*=contato]')
