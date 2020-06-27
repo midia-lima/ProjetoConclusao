84465b00d61491dff2d6b21671f4514067690d1c
midia-lima <midialp2@gmail.com>
2020-06-27 13:38:49 -0300

    reset formulário

--- js/main.js
+++ js/main.js
@@ -13,16 +13,19 @@ ativaScrollSuave('a[href*=sobre]');
 ativaScrollSuave('a[href*=sobre-palestrantes]');
 ativaScrollSuave('a[href*=contato]');
 
+const form = document.querySelector("#contato");
 const nome = document.querySelector('input[name="nome"]');
 const email = document.querySelector('input[name="email"]');
 const btn = document.querySelector("#botao-final");
+const mgserro = document.querySelector("#mensagem-erro");
 
 btn.addEventListener('click', function () {
     event.preventDefault();
     if (nome.value.length == 0 || email.value.length == 0) {
-        alert("Por favor preecha todos os campos");
-    }
-    else {
+        mgserro.innerHTML = "Por favor preecha todos os campos"
+    } else {
         alert("Incrição efetuada com sucesso!");
+        mgserro.innerHTML = "";
     }
+    form.reset();
 })
